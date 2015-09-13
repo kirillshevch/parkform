2d40b5b1861335344a9e6a4e03025ed54718835c
Alex Babakhanov <babakhanov1@gmail.com>
2015-09-13 15:33:08 +0300

    seeder

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,3 +1,5 @@
 class Form < ActiveRecord::Base
   belongs_to :user
+  has_many :questions, dependent: :destroy
+  has_many :responses, dependent: :destroy
 end
--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,10 +1,32 @@
 FactoryGirl.define do
   factory :form do
-    start_at "2015-09-13 11:43:02"
-finish_at "2015-09-13 11:43:02"
-title "MyString"
-description "MyText"
-user nil
-  end
+    start_at { Time.now } 
+    finish_at { 1.year.from_now } 
+    title { Faker::Lorem.sentence } 
+    description { Faker::Lorem.sentence  }
+    questions {
+      create_list(:question, 3, :radio) + 
+      create_list(:question, 3, :checkbox) +
+      create_list(:question, 3, :slider) +
+      create_list(:question, 3, :text)
+    }
+    user
 
+    trait :with_responses do
+      responses { create_list(:response, 12) }
+      after(:create) do |form|
+        form.responses.each do |r|
+          data = []
+          form.questions.each do |q|
+            value = Faker::Lorem.words(3).join(" ")
+            value = q.data.sample if q.question_type.to_s == "checkbox" || q.question_type.to_s == "radio"
+            value = rand(100..1000) if q.question_type.to_s == "slider"
+            data << [ q.id, value ]        
+          end 
+          r.update!(data: data)
+        end
+      end
+    end 
+
+  end
 end
